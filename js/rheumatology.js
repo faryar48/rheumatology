865b1c3b5d76f9812d3b656a756ddc783f0ae138
Faryar Ghaemi <faryarg88@gmail.com>
2015-05-06 11:45:21 +1000

    working on the main page

--- js/rheumatology.js
+++ js/rheumatology.js
@@ -1,10 +1,11 @@
   $(document).ready(function() {
+    
     $('#login-button').on('click', function () {
       if ($('#inputPassword').val() === '20rheumatology15') {
-         window.location.href = "home.html"; 
+         window.location.href = "main.html"; 
       } else { 
           console.log('no cars go'); 
-          $('#error-message').text("Incorrect password.Please try again.")
+          $('#error-message').text("Incorrect password, please try again.")
         
       }
 
